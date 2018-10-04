56b64e4b2d23beb95aa820d62176c6440af123be
user <valiagagina@gmail.com>
2018-10-04 12:00:23 +0300

    20181004_12_00

---
+++ dialogs.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+
+a = input("Cien.liet.,lūdzu, ievadi skaitli:")
+a = int(a)
+print("Liet., Tu esi ievadījis skaitli: %d"%(a))
+aa = a * a
+print("Tavs skaitlis kvadrātā ir: %d%(aa))
+print("Tavs skaitlis kvadrātā ir: %d"%(aa))
+

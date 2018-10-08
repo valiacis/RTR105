8a4c9f3d9e8be60412d460f24904012e28f74b48
user <valiagagina@gmail.com>
2018-10-08 15:29:35 +0300

    20181008_15_30

---
+++ dgr_20181008.py
@@ -0,0 +1,157 @@
+Python 2.7.12 (default, Dec  4 2017, 14:50:18) 
+[GCC 5.4.0 20160609] on linux2
+Type "copyright", "credits" or "license()" for more information.
+>>> print(123)
+123
+>>> x = 12.2
+>>> x
+12.2
+>>> x = 12.2
+>>> x = 100
+>>> x
+100
+>>> x = 2
+>>> x = x + 2
+>>> print(x)
+4
+>>> x = 3.9 *x *(1-x)
+>>> x
+-46.8
+>>> ddd = 1 + 4
+>>> print(ddd)
+5
+>>> print(eee)
+
+Traceback (most recent call last):
+  File "<pyshell#13>", line 1, in <module>
+    print(eee)
+NameError: name 'eee' is not defined
+>>> eee = 'hello' + 'there'
+>>> eee
+'hellothere'
+>>> print(float(99) + 100)
+199.0
+>>> i = 42
+>>> type(i)
+<type 'int'>
+>>> print(f)
+
+Traceback (most recent call last):
+  File "<pyshell#19>", line 1, in <module>
+    print(f)
+NameError: name 'f' is not defined
+>>> f = float(i)
+>>> print (f)
+42.0
+>>> print(10/2)
+5
+>>> print(9/2)
+4
+>>> print(0.99/100)
+0.0099
+>>> sval='123'
+>>> type(sval)
+<type 'str'>
+>>> print(sval + 1)
+
+Traceback (most recent call last):
+  File "<pyshell#27>", line 1, in <module>
+    print(sval + 1)
+TypeError: cannot concatenate 'str' and 'int' objects
+>>> ival = int(sval)
+>>> type(ival)
+<type 'int'>
+>>> print(ival + 1)
+124
+>>> nvs = 'hello bob'
+>>> niv = int(nsv)
+
+Traceback (most recent call last):
+  File "<pyshell#33>", line 1, in <module>
+    niv = int(nsv)
+NameError: name 'nsv' is not defined
+>>> nam = input('Who are you?'°
+	    
+SyntaxError: invalid syntax
+>>> print('Welcome', nam)
+
+Traceback (most recent call last):
+  File "<pyshell#35>", line 1, in <module>
+    print('Welcome', nam)
+NameError: name 'nam' is not defined
+>>> nam = input('Who are you?')
+Who are you?
+
+Traceback (most recent call last):
+  File "<pyshell#36>", line 1, in <module>
+    nam = input('Who are you?')
+  File "<string>", line 0
+    
+    ^
+SyntaxError: unexpected EOF while parsing
+>>> print('Welcome'nam)
+SyntaxError: invalid syntax
+>>> inp = input('Europe floor?')
+Europe floor?usf = int(inp) + 1
+
+Traceback (most recent call last):
+  File "<pyshell#38>", line 1, in <module>
+    inp = input('Europe floor?')
+  File "<string>", line 1
+    usf = int(inp) + 1
+        ^
+SyntaxError: invalid syntax
+>>> inp = input('Europe floor?)
+	    
+SyntaxError: EOL while scanning string literal
+>>> inp = input('Europe floor?')
+Europe floor?
+
+Traceback (most recent call last):
+  File "<pyshell#40>", line 1, in <module>
+    inp = input('Europe floor?')
+  File "<string>", line 0
+    
+    ^
+SyntaxError: unexpected EOF while parsing
+>>> 
+>>> usf = int(inp) + 1
+
+Traceback (most recent call last):
+  File "<pyshell#42>", line 1, in <module>
+    usf = int(inp) + 1
+NameError: name 'inp' is not defined
+>>> inp = input('Europe floor?')
+Europe floor? inp = input('Europe floor?')
+
+Traceback (most recent call last):
+  File "<pyshell#43>", line 1, in <module>
+    inp = input('Europe floor?')
+  File "<string>", line 1
+    inp = input('Europe floor?')
+        ^
+SyntaxError: invalid syntax
+>>> print('US floor', usf)
+
+Traceback (most recent call last):
+  File "<pyshell#44>", line 1, in <module>
+    print('US floor', usf)
+NameError: name 'usf' is not defined
+>>> pwd
+
+Traceback (most recent call last):
+  File "<pyshell#45>", line 1, in <module>
+    pwd
+NameError: name 'pwd' is not defined
+>>> inp = input('Europe floor?')
+Europe floor?78
+>>> usf = int(inp) + 1
+>>> print('US floor', usf)
+('US floor', 79)
+>>> inp
+78
+>>> usf
+79
+>>> 
+
+
